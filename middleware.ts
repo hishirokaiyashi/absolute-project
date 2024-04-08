b909858b1bbc25ce505d247ce44b1a779f866451
Tran Giao <giao@vietswiss.com>
2024-04-08 18:28:48 +0700

    fix:fix some errors

---
+++ middleware.ts
@@ -0,0 +1,39 @@
+import { match } from '@formatjs/intl-localematcher';
+import Negotiator from 'negotiator';
+import { NextResponse } from 'next/server';
+
+let headers = { 'accept-language': 'en-US,en;q=0.5' };
+let languages = new Negotiator({ headers }).languages();
+let locales = ['en-US', 'nl-NL', 'nl'];
+let defaultLocale = 'en-US';
+match(languages, locales, defaultLocale); // -> 'en-US'
+// Get the preferred locale, similar to the above or using a library
+function getLocale(request:any) {
+  const acceptLanguage = request.headers.get('Accept-Language');
+}
+
+export function middleware(request:any) {
+  // Check if there is any supported locale in the pathname
+  const { pathname } = request.nextUrl;
+  const pathnameHasLocale = locales.some(
+    locale => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`
+  );
+
+  if (pathnameHasLocale) return;
+
+  // Redirect if there is no locale
+  const locale = getLocale(request);
+  request.nextUrl.pathname = `/${locale}${pathname}`;
+  // e.g. incoming request is /products
+  // The new URL is now /en-US/products
+  return NextResponse.redirect(request.nextUrl);
+}
+
+export const config = {
+  matcher: [
+    // Skip all internal paths (_next)
+    '/((?!_next).*)'
+    // Optional: only run on root (/) URL
+    // '/'
+  ]
+};
---
+++ next.config.js
@@ -0,0 +1,16 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     i18n: {
+//         defaultLocale: 'en',
+//         locales: ['en', 'de'],
+//       },
+// };
+
+// export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    /* config options here */
+  }
+   
+  module.exports = nextConfig
